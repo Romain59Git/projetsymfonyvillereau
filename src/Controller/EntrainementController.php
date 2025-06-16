c0a64bba421cda2ec276b300063853083ad6a8e7
Romain59Git <romain.carlier6@orange.fr>
2025-06-16 23:50:27 +0200

    add new controller/templates + add footer

---
+++ src/Controller/EntrainementController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class EntrainementController extends AbstractController
+{
+    #[Route('/entrainement', name: 'app_entrainement')]
+    public function index(): Response
+    {
+        return $this->render('entrainement/index.html.twig', [
+            'controller_name' => 'EntrainementController',
+        ]);
+    }
+}
