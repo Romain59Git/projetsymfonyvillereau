2152b04756d1d352af4597c890eeea877a54a5e3
Romain59Git <romain.carlier6@orange.fr>
2025-06-19 20:09:00 +0200

    feat(avis): implement avis feature with form and display

--- src/Controller/AvisController.php
+++ src/Controller/AvisController.php
@@ -2,17 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Avis;
+use App\Repository\AvisRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class AvisController extends AbstractController
+class AvisController extends AbstractController
 {
     #[Route('/avis', name: 'app_avis')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, AvisRepository $avisRepository): Response
     {
+        // Gérer la soumission du formulaire
+        if ($request->isMethod('POST')) {
+            $avis = new Avis();
+            $avis->setName($request->request->get('name'))
+                 ->setRating((int) $request->request->get('rating'))
+                 ->setComment($request->request->get('comment'));
+
+            $entityManager->persist($avis);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Merci pour votre avis !');
+
+            return $this->redirectToRoute('app_avis');
+        }
+
+        // Récupérer tous les avis pour l'affichage
+        $avis = $avisRepository->findBy([], ['createdAt' => 'DESC']);
+
         return $this->render('avis/index.html.twig', [
-            'controller_name' => 'AvisController',
+            'avis' => $avis,
         ]);
     }
 }
---
+++ src/Repository/AvisRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Avis;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Avis>
+ *
+ * @method Avis|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Avis|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Avis[]    findAll()
+ * @method Avis[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AvisRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Avis::class);
+    }
+
+    /**
+     * @return Avis[] Returns an array of Avis objects
+     */
+    public function findLatest(): array
+    {
+        return $this->createQueryBuilder('a')
+            ->orderBy('a.createdAt', 'DESC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult();
+    }
+} 
